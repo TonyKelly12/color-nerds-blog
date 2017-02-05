1c56bc01891981ac8b0718ddeb587a7a665aac4e
Tony Kelly <Tonykelly1286@gmail.com>
2017-02-05 02:36:01 -0600

    workflow setup

--- color_nerds_blog/main.py
+++ color_nerds_blog/main.py
@@ -4,11 +4,36 @@ Created on Jan 31, 2017
 @author: toned_000
 '''
 import webapp2
+import jinja2
 
-class MainPage(webapp2.RequestHandler):
+form_html = """
+<form>
+<h2>Add a Food</h2>
+<input type = "text" name ="food">
+<input type = "hidden"
+<button>Add</button>
+</form>
+"""
+
+#template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+#jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+
+class Handler(webapp2.RequestHandler):
     
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+  #  def render_str(self, template, **params):
+  #      t = jinja_env.get_template(template)
+  #      return t.render(params)
+    
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+    
+class MainPage(Handler):
     def get(self):
-        self.response.out.write("<h1>Hello, World!</h1>")
+        self.write(form_html)
+
 
 app = webapp2.WSGIApplication([
     ("/", MainPage)
