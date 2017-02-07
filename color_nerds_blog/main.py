19fd2d6547b918101ad7d1b9daf435d29823f933
Tony Kelly <Tonykelly1286@gmail.com>
2017-02-06 23:25:30 -0600

    classes added

--- color_nerds_blog/main.py
+++ color_nerds_blog/main.py
@@ -3,36 +3,64 @@ Created on Jan 31, 2017
 
 @author: toned_000
 '''
+import os
 import webapp2
 import jinja2
 
-form_html = """
-<form>
-<h2>Add a Food</h2>
-<input type = "text" name ="food">
-<input type = "hidden"
-<button>Add</button>
-</form>
+from google.appengine.ext import db
+
+hidden_html = """
+<input type="hidden" name="food" value="%s">
 """
 
-#template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-#jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+item_html = "<li>%s</li>"
+
+shopping_list_html = """
+<br>
+<br>
+<h2>Shopping List</h2>
+<ul>
+%s
+</ul>
+
+"""
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape= True)
 
 class Handler(webapp2.RequestHandler):
     
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-  #  def render_str(self, template, **params):
-  #      t = jinja_env.get_template(template)
-  #      return t.render(params)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
     
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
     
+class Post(db.Model):
+    user_id = db.IntegerProperty(required = True)
+    title = db.StringProperty(required = True)
+    post = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    hashtag = db.StringProperty(required = False)
+    likes = db.IntegerProperty(required = True)
+
+class User(db.Model):
+    u_id = db.IntegerProperty(required = True)
+    rName = db.StringProperty(required = True)
+    sName = db.StringProperty(required = True)
+    signup = db.DateTimeProperty(auto_now_add = True)
+    email = db.EmailProperty(required = True)
+
 class MainPage(Handler):
     def get(self):
-        self.write(form_html)
+        self.render("index.html")
+    
+    def post(self):
+        title = self.request.get
 
 
 app = webapp2.WSGIApplication([
