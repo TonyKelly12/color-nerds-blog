5b962633c257a6c060aa5438422ad3f66ce3a1aa
Tony Kelly <Tonykelly1286@gmail.com>
2017-02-15 21:40:01 -0600

    blog shell working

--- color_nerds_blog/main.py
+++ color_nerds_blog/main.py
@@ -4,30 +4,21 @@ Created on Jan 31, 2017
 @author: toned_000
 '''
 import os
+import re
 import webapp2
 import jinja2
 
 from string import letters
 from google.appengine.ext import db
-#from jinja2 import Environment, PackageLoader, select_autoescape
-hidden_html = """
-<input type="hidden" name="food" value="%s">
-"""
-
-item_html = "<li>%s</li>"
-
-shopping_list_html = """
-<br>
-<br>
-<h2>Shopping List</h2>
-<ul>
-%s
-</ul>
-
-"""
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),autoescape= True)
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+GOOGLE_APPLICATION_CREDENTIALS = "E:\Color Nerds\Web Design\GitHub Repo's\Blogg App\color_nerds_blog\GOOGLE_APPLICATION_CREDENTIALS\Color Nerds Blog-5c4bbb37a2fe.json"
+
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
 class Handler(webapp2.RequestHandler):
     
@@ -37,50 +28,52 @@ class Handler(webapp2.RequestHandler):
     def render_str(self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
-    
+
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
+
+def render_post(response, post):
+    response.out.write('<b>' + post.title + '</b><br>')
+    response.out.write(post.content)
     
 class Post(db.Model):
-    user_id = db.IntegerProperty(required = True)
-    title = db.StringProperty(required = True)
-    bpost = db.TextProperty(required = True)
-    created = db.DateTimeProperty(auto_now_add = True)
-    last_mod = db.DateTimeProperty(auto_now = True)
-    hashtag = db.StringProperty(required = False)
-    likes = db.IntegerProperty(required = True)
+    #user_id = db.IntegerProperty(required = True)
+    title = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+    last_mod = db.DateTimeProperty(auto_now=True)
+    #hashtag = db.StringProperty(required = False)
+    #likes = db.IntegerProperty(required = True)
 
-class User(db.Model):
-    u_id = db.IntegerProperty(required = True)
-    rName = db.StringProperty(required = True)
-    sName = db.StringProperty(required = True)
-    signup = db.DateTimeProperty(auto_now_add = True)
-    email = db.EmailProperty(required = True)
+    def render(self): #NOT Working
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p=self)
 
-class Index(Handler):
-    def render_front(self, title="", bpost="", error=""):
-        self.render("blog.html", title = title, bpost = bpost, error = error)
+#class User(db.Model):
+#    u_id = db.IntegerProperty(required=True)
+#    rName = db.StringProperty(required=True)
+#    sName = db.StringProperty(required=True)
+#    signup = db.DateTimeProperty(auto_now_add=True)
+#    email = db.EmailProperty(required=True)
 
-    def blog_key(name = 'default'):
-        return db.Key.from_path('blogs', name)
 
-    def get(self):
-        self.render_front()
 
-   
+class Index(Handler): #This is probably the problem
+   def render_front(self):
+      self.render("index.html")
 
-class Login(Handler):
-    def get(self):
-        self.render("login.html")
+   def get(self):
+     self.render_front()
 
-class Signup(Handler):
-    def get(self):
-        self.render("signup.html")
+def blog_key(name='default'):
+    return db.Key.from_path('blogs', name)
+
+
 
 class BlogPage(Handler):
     def get(self):
         posts = db.GqlQuery("select * from Post order by created desc limit 10")
-        self.render("blog.html", posts = posts)
+        self.render("blog.html", posts=posts)
 
 class PostPage(Handler):
     def get(self, post_id):
@@ -90,7 +83,7 @@ class PostPage(Handler):
         if not post:
             self.error(404)
             return
-        self.render("post.html", post = post)
+        self.render("permalink.html", post=post)
 
 class NewPost(Handler):
     def get(self):
@@ -98,25 +91,33 @@ class NewPost(Handler):
 
     def post(self):
         title = self.request.get("title")
-        bpost = self.request.get("post")
+        content = self.request.get("content")
 
-        if title and bpost:
-            P = Post(parent = blog_key(), subject = subject, content = content)
+        if title and content:
+            p = Post(parent=blog_key(), title=title, content=content)
             p.put()
             self.redirect('/blog/%s' % str(p.key().id()))
         else:
             error = "You need a title and a post"
-            self.render("newpost.html", subject = subject, content = content, error = error)
+            self.render("newpost.html", title=title, content=content, error=error)
 
+
+class Login(Handler):
+    def get(self):
+        self.render("login.html")
+
+class Signup(Handler):
+    def get(self):
+        self.render("signup.html")
 app = webapp2.WSGIApplication([
     ("/", Index),
     ("/signup", Signup),
     ("/login", Login),
-    ("/blog", BlogPage),
-    ("/blog/post/([0-9]+)", PostPage),
+    ("/blog/?", BlogPage),
+    ("/blog/([0-9]+)", PostPage),
     ("/blog/newpost", NewPost)
 
 ],
-debug = True)
+debug=True)
 
 
